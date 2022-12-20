b031d092c7e0bb7fca2ad580e055cf3478d52a39
FranQuintelaVela <fraquivel@gmail.com>
2022-12-20 12:18:33 +0100

    Removed closeOnEscapeKeyDown usage

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,18 +4,18 @@ import { CSSTransition } from "react-transition-group";
 import "./Modal.css";
 
 const Modal = props => {
-  const closeOnEscapeKeyDown = e => {
-    if ((e.charCode || e.keyCode) === 27) {
-      props.onClose();
-    }
-  };
+  // const closeOnEscapeKeyDown = e => {
+  //   if ((e.charCode || e.keyCode) === 27) {
+  //     props.onClose();
+  //   }
+  // };
 
-  useEffect(() => {
-    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
-    return function cleanup() {
-      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
-    };
-  }, []);
+  // useEffect(() => {
+  //   document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+  //   return function cleanup() {
+  //     document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+  //   };
+  // }, []);
 
   return ReactDOM.createPortal(
     <CSSTransition
